1df3237f14aed0499104c46badf9a37d12d78a49
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-01-25 21:05:17 +0200

    Second  task

--- main.py
+++ main.py
@@ -27,4 +27,21 @@ def first_task():
 
     plt.show()
 
+
+def second_task():
+    # First dataframe and see age categories
+    dataframe = pandas.read_csv('titles.csv')
+    age_categories = dataframe[dataframe['type'] == 'SHOW']
+
+    # Second dataframe and set labels
+    dataframe2 = age_categories.groupby('age_certification')['age_certification'].count()
+    labels = set(age_categories[age_categories['age_certification'].notnull()]['age_certification'])
+
+    plt.pie(dataframe2, labels=labels)
+
+    plt.title('Age ratings')
+    plt.show()
+
+
 first_task()
+second_task()
